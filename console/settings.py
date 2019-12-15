7c8342740a7468484b7a13b053286518d22b2b46
chris chang <chiachun0920@gmail.com>
2019-12-15 09:43:54 +0000

    Add: create auth safer and user model

--- console/settings.py
+++ console/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'security.apps.SecurityConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- security/models.py
+++ security/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=32)
+
+    created_date = models.DateTimeField(auto_now=True)
+
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'user'
+
+
+class AuthSafer(models.Model):
+    username = models.CharField(max_length=32)
+
+    encrypted_password = models.TextField()
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created_date = models.DateTimeField(auto_now=True)
+
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'auth_safter'
+
